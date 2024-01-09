ba53f841c47cdd4f1c41b745a7d8f7231f577bde
Snowsore <snow_sore@hotmail.com>
2024-01-09 11:12:49 +0800

    Add result page

--- pages/fortune/fortune-result.vue
+++ pages/fortune/fortune-result.vue
@@ -1,16 +1,35 @@
 <template>
 	<view class="body">
-		<image class="full-width" mode="widthFix" src="../../static/result-page.png"></image>
+		<!-- <image class="full-width" mode="widthFix" src="../../static/result-page.png" /> -->
+		{{ fateData }}
 	</view>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			fateData: ''
+		};
 	},
-	onLoad(option) {},
-	methods: {}
+	onLoad(option) {
+		this.updateData();
+	},
+	methods: {
+		async updateData() {
+			const { serviceId } = this.options;
+
+			const res = await uni.request({
+				url: `/service/fortune_service/${serviceId}`
+			});
+
+			if (res.statusCode >= 300) return;
+
+			const { data } = res;
+
+			this.fateData = data;
+		}
+	}
 };
 </script>
 
